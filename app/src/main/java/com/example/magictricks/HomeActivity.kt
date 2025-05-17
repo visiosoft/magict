076a9fbd2e5f6f88f39f4daaa074ebe3e0a83acf
Zulfiqar <zulfiqar@mypaperlessoffice.org>
2025-05-18 03:33:46 +0500

    videos

--- app/src/main/java/com/example/magictricks/HomeActivity.kt
+++ app/src/main/java/com/example/magictricks/HomeActivity.kt
@@ -164,10 +164,10 @@ class HomeActivity : AppCompatActivity() {
         val trendingTricks = listOf(
             Trick(
                 id = "1",
-                title = "Amazing Card Vanish",
-                description = "Learn this impressive card vanish trick that will leave your audience amazed. Perfect for beginners!",
-                videoUrl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-                thumbnailUrl = "https://i3.ytimg.com/vi/HzxFZQSA-Kc/hqdefault.jpg",
+                title = "Amazing Donut Vanished",
+                description = "Learn this impressive donut vanish trick that will leave your audience amazed. Perfect for beginners!",
+                videoUrl = "https://raw.githubusercontent.com/visiosoft/videostreaming/main/donut.mp4",
+                thumbnailUrl = "https://i.ytimg.com/vi/cqNSka76wBA/hq720.jpg",
                 duration = 180,
                 categories = listOf("Card Tricks"),
                 isPro = false,
@@ -176,9 +176,32 @@ class HomeActivity : AppCompatActivity() {
             Trick(
                 id = "2",
                 title = "Mind-Blowing Coin Magic",
-                description = "Master the art of coin manipulation with this advanced technique. A must-learn for coin magic enthusiasts.",
-                videoUrl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-                thumbnailUrl = "https://i3.ytimg.com/vi/HzxFZQSA-Kc/hqdefault.jpg",
+                description = "IMPOSSIBLE Coin Tricks Anyone Can Do",
+                videoUrl = "https://raw.githubusercontent.com/visiosoft/videostreaming/main/2.mp4",
+                thumbnailUrl = "https://i.ytimg.com/vi/0V9qnde2Rcc/hq720.jpg",
+                duration = 240,
+                categories = listOf("Coin Magic"),
+                isPro = true,
+                isFeatured = true
+            ),
+            Trick(
+                id = "3",
+                title = "Funny Magic Tricks Secret Revealed",
+                description = "Pencil TricksAnyone Can Do",
+                videoUrl = "https://raw.githubusercontent.com/visiosoft/videostreaming/main/3.mp4",
+                thumbnailUrl = "https://i.ytimg.com/vi/yy1u9pHDm5k/hq720_2.jpg",
+                duration = 240,
+                categories = listOf("Coin Magic"),
+                isPro = true,
+                isFeatured = true
+            )
+            ,
+            Trick(
+                id = "4",
+                title = "Funny Magic Tricks And DIY Illusions That You Can Do",
+                description = "3 Magic Tricks Anyone Can Do",
+                videoUrl = "https://raw.githubusercontent.com/visiosoft/videostreaming/main/4.mp4",
+                thumbnailUrl = "https://raw.githubusercontent.com/visiosoft/videostreaming/main/4.jpg",
                 duration = 240,
                 categories = listOf("Coin Magic"),
                 isPro = true,
